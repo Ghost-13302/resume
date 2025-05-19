a0cc3da3c5cc84c85c941a1e615131bed7311c96
Sanket <sanketjadhao2002@gmail.com>
2025-05-18 23:03:59 -0400

    feat: Implement initial portfolio website

---
+++ tailwind.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"], // Make sure this line includes .astro and any other file types you use classes in
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
