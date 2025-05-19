30b28ca35a93307d1fc68b2ebd084c9adcf82e3f
Sanket <sanketjadhao2002@gmail.com>
2025-05-18 23:31:42 -0400

    fix: css not deploying in build

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"], // Make sure this line includes .astro and any other file types you use classes in
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
